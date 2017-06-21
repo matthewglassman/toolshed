33d81f61519390883144faac63bc03fdd6f7da0a
matthewglassman <matthewglassmn78@gmail.com>
2017-06-20 20:12:40 -0400

    server.js altered

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var expressValidator = require ('express-validator');
 
 // Require User Schema
 var User = require ('./client/models/User.js');
-var Group = require ('./client/models/Group.js');
+var Group = require ('./client/models/group.js');
 
 //Require Tool Schema
 var Tool = require ('./client/models/Tool.js');
